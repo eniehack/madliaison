40ebdcf21bc7acaacb468edbfdd816cc1032600d
Nakaya <eniehack@outlook.jp>
2023-01-18 02:34:22 +0900

    first commit

---
+++ lib/madliaison.ex
@@ -0,0 +1,18 @@
+defmodule Madliaison do
+  @moduledoc """
+  Documentation for `Madliaison`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Madliaison.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/madliaison_test.exs
@@ -0,0 +1,8 @@
+defmodule MadliaisonTest do
+  use ExUnit.Case
+  doctest Madliaison
+
+  test "greets the world" do
+    assert Madliaison.hello() == :world
+  end
+end
